c115f2dc2bf36aaac434ce1906d3e58a1668f8c5
SaadaBrahim <saada.brahimbryan.studente@itispaleocapa.it>
2026-01-30 11:50:41 +0000

    test

---
+++ Computer.java
@@ -0,0 +1,27 @@
+public class Computer{
+    private String marca;
+    private String processore;
+    private int ram; // GB
+    
+    //getter&setter
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getProcessore() {
+        return processore;
+    }
+    public void setProcessore(String processore) {
+        this.processore = processore;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+    
+}
